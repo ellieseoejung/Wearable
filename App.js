b6f661c7edb92282ef8bd52e70bb4cea8fb42f63
Sean Lachhander <slachhander@albany.edu>
2017-06-27 19:46:18 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+    Component
+} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 import Splash from './Splash';
 import Login from './Login';
 export default class App extends React.Component {
-  render() {
-    return (
-      <Login />
-    );
-  }
+    render() {
+        return ( <
+            Login / >
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF',
 
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    }
 });
