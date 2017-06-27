a0d6f2f5fac384d141122ef917fae09bc3f6fd44
Sean Lachhander <slachhander@albany.edu>
2017-06-27 18:14:09 -0400

    Add files via upload
    Update: These files give the app a Splash Screen only.

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Splash from './Splash';
+export default class App extends React.Component {
+  render() {
+    return (
+      <Splash />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
